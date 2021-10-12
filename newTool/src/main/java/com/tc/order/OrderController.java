6fbde10aeb0e0c9af8c843f16f1c4c69af7ef81a
longxiangfu <1565267514@qq.com>
2021-10-12 17:33:52 +0800

    apiggs

---
+++ newTool/src/main/java/com/tc/order/OrderController.java
@@ -0,0 +1,49 @@
+package com.tc.order;
+
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import java.util.Date;
+
+/**
+ * 订单管理
+ */
+@RestController
+@RequestMapping("/order")
+public class OrderController {
+
+    /**
+     * 根据单号获取的订单
+     * @param orderNo 订单号
+     * @param status 订单状态
+     * @return 订单对象
+     */
+    @GetMapping("get")
+    public OrderInfoBo getOrder(
+            @RequestParam(defaultValue = "2020111111") @NotBlank String orderNo,
+            @RequestParam @NotNull Integer status
+    ){
+        OrderInfoBo bo = new OrderInfoBo();
+        bo.setOrderNo("2020111111");
+        bo.setStatus(1);
+        bo.setCreateDate(new Date());
+        return bo;
+    }
+
+    /**
+     * 创建订单
+     * @param orderInfo 订单信息
+     * @return 是否添加成功
+     */
+    @PostMapping("add")
+    public OrderInfoBo add(@RequestBody OrderInfoVo orderInfo){
+        OrderInfoBo bo = new OrderInfoBo();
+        bo.setOrderNo("2020111111");
+        bo.setStatus(1);
+        bo.setCreateDate(new Date());
+        return bo;
+    }
+
+
+}
---
+++ newTool/src/main/java/com/tc/order/OrderInfoBo.java
@@ -0,0 +1,60 @@
+package com.tc.order;
+
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import java.util.Date;
+
+/**
+ * 订单Bo
+ */
+public class OrderInfoBo {
+
+    /**
+     * 订单号
+     */
+    @NotBlank
+    private String orderNo;
+    /**
+     * 订单状态
+     */
+    @NotNull
+    private Integer status;
+    /**
+     * 创建日期
+     */
+    @NotNull
+    private Date createDate;
+
+    public String getOrderNo() {
+        return orderNo;
+    }
+
+    public void setOrderNo(String orderNo) {
+        this.orderNo = orderNo;
+    }
+
+    public Integer getStatus() {
+        return status;
+    }
+
+    public void setStatus(Integer status) {
+        this.status = status;
+    }
+
+    public Date getCreateDate() {
+        return createDate;
+    }
+
+    public void setCreateDate(Date createDate) {
+        this.createDate = createDate;
+    }
+
+    @Override
+    public String toString() {
+        return "OrderInfoVo{" +
+                "orderNo='" + orderNo + '\'' +
+                ", status=" + status +
+                ", createDate=" + createDate +
+                '}';
+    }
+}

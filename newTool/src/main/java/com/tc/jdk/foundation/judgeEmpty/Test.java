353c3710d315bfde29930ac2c45c922d942e1f73
lxf <1565267514@qq.com>
2025-07-27 21:51:21 +0800

    判空

--- newTool/src/main/java/com/tc/jdk/foundation/judgeEmpty/Test.java
+++ newTool/src/main/java/com/tc/jdk/foundation/judgeEmpty/Test.java
@@ -4,37 +4,37 @@ import org.apache.commons.collections4.CollectionUtils;
 import org.apache.commons.collections4.MapUtils;
 import org.apache.commons.lang3.StringUtils;
 
-import java.util.ArrayList;
-import java.util.List;
-
-import java.util.HashMap;
-import java.util.Map;
+import java.util.*;
 
 /**
  * 判空
  * 对象  字符串   列表   Map
  */
 public class Test {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
         // 对象
-        JudgeEntity judgeEntity = null;
-        if (judgeEntity == null) {
-            System.out.println("空");
-        }
+//        JudgeEntity judgeEntity = new JudgeEntity();
+//        JudgeEntity judgeEntity = null;
+//        if (judgeEntity == null) {
+//            System.out.println("空");
+//        }
+//        judgeEntity = Optional.ofNullable(judgeEntity).orElseThrow(Exception::new);
+//        System.out.println("judgeEntity:" + judgeEntity);
 
-        // 字符串
-        String str = "";
-        if (StringUtils.isBlank(str)) {
-            System.out.println("空");
-        }
-
-        // 列表 java8新特性–双括号初始化
-        List list = new ArrayList<Integer>(){{add(1); add(2);}};
-        System.out.println(CollectionUtils.isEmpty(list));
-
-        // Map  java8新特性–双括号初始化
-        Map map = new HashMap<String, Integer>(){{put("a",1); put("b",2);}};
-        System.out.println(MapUtils.isEmpty(map));
+//        // 字符串
+//        String str = "";
+//        if (StringUtils.isBlank(str)) {
+//            System.out.println("空");
+//        }
+//
+//        // 列表 java8新特性–双括号初始化
+//        List list = new ArrayList<Integer>(){{add(1); add(2);}};
+//        System.out.println(CollectionUtils.isEmpty(list));
+//
+//        // Map  java8新特性–双括号初始化
+//        Map map = new HashMap<String, Integer>(){{put("a",1); put("b", 2);}};
+//        Map map = new HashMap<String, Integer>();
+//        System.out.println(MapUtils.isEmpty(map));
 
 
     }
